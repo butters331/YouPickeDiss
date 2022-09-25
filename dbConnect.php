f1c91c15676beb4283f05d20b1b96dfc9b605ce4
Jack <jack.butterfield@student.manchester.ac.uk>
2022-09-25 20:26:31 +0100

    minor bug fixes to dbconnect and products.php

--- dbConnect.php
+++ dbConnect.php
@@ -5,12 +5,12 @@ $dbPassword = "Youpickediss10$";
 $dbName = "u268055314_ypd";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbName);
+$conn = new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
 
 // Check connection
 // Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
 ?>
+
--- products.php
+++ products.php
@@ -23,7 +23,7 @@
                 //get products from db and count of them
                 $getProductsSQL = 'SELECT * FROM Products;';
                 $products = mysqli_query($conn, $getProductsSQL);
-                $noOfProducts = mysqli_num_row($products);
+                $noOfProducts = mysqli_num_rows($products);
 
                 //init global variables
                 $count = 0;
@@ -36,12 +36,12 @@
                         $imgSQL = "SELECT Path FROM ProductImgs WHERE prodID=".$productId.";";
                         $prodImgs = mysqli_query($conn, $imgSQL);
                         $prodImgArray = mysqli_fetch_all($prodImgs);
-                        $noOfImgs = mysqli_num_row($prodImgs);
+                        $noOfImgs = mysqli_num_rows($prodImgs);
                         $imgCounter = 0;
                         if ($noOfImgs != 0){
                             echo '<td width="25%"><div class="container productContainer"><div id="product'.$count.'" class="carousel carousel-dark slide productThumbnail" data-bs-ride="carousel"><div class="carousel-indicators">';
                             
-                            for ($x=0; $x < $noOfProducts; $x++){
+                            for ($x=0; $x < $noOfImgs; $x++){
                                 if ($x == 0){
                                     echo '<button type="button" data-bs-target="#product'.$count.'" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>';
                                 }
@@ -61,9 +61,10 @@
                                 echo ''.$imgPath.'" class="d-block w-100 productThumbnailImage" alt="..."></a></div>';
                                 $imgCounter++;
                             }//while imgs to paste in carousel
+
                             echo '</div></div></div><div class="productSubtext">'.$row['name'].'<br /> £'.$row['price'].'</div><div class="productAddToBasket"><button type="button" class="btn btn-outline-success">Add To Basket</button></div></td>';
                             $count++;
-                            if (count % 4 == 0){
+                            if ($count % 4 == 0){
                                 echo '</tr><tr>';
                             }
 
