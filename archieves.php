8f3584fb91e1ac0ded914f7d111fa09d7784f9cd
Jack <jack.butterfield@student.manchester.ac.uk>
2023-01-27 21:15:20 +0000

    changed archives to be a gallery view

--- archieves.php
+++ archieves.php
@@ -16,7 +16,10 @@
     <script type="text/javascript" src="cart.js"></script>
     <script type="text/javascript" src="search.js"></script>
 
-    <?php
+    <div class="container pageInfo">
+    <table style="width:100%;min-height:450px;">
+        <tr style="height:40%;">
+        <?php
         include_once 'dbConnect.php';
         $getImages = "SELECT * FROM ArchiveImgs ORDER BY DateAdded DESC";
         $images = mysqli_query($conn, $getImages);
@@ -28,49 +31,35 @@
                 $imageArray = mysqli_fetch_assoc($images);
                 //paste IMGs
                 echo '
-    <div class="container pageInfo">
-        <img src="'.$imageArray["Path"].'" class="d-block w-100" alt="..." data-bs-toggle="modal" data-bs-target="#picture'.$count.'Modal" />
-    </div>';
-
-                echo '
-    <div class="modal fade" id="picture'.$count.'Modal" tabindex="-1" data-bs-backdrop="static" aria-hidden="true">
-        <div class="modal-dialog modal-xl modal-fullscreen-lg-down">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <td style="width:33%;">
+                <img src="'.$imageArray["Path"].'" class="d-block w-100" alt="..." data-bs-toggle="modal" data-bs-target="#picture'.$count.'Modal" />
+                <div class="modal fade" id="picture'.$count.'Modal" tabindex="-1" data-bs-backdrop="static" aria-hidden="true">
+                    <div class="modal-dialog modal-xl modal-fullscreen-lg-down">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <img src="'.$imageArray["Path"].'" class="d-block w-100" alt="..." />
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="modal-body">
-                    <img src="'.$imageArray["Path"].'" class="d-block w-100" alt="..." />
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Close</button>
-                </div>
-            </div>
-        </div>
-    </div>';
+            </td>';
+                if (($count % 3 == 2) && ($count != $noOfImages - 1)){
+                    echo '</tr><tr style="height:40%;">';
+                }
             }//for
-    //         echo '
-    // <div class="container pageInfo" style="padding-bottom: 1em;">
-    //     <img src="'.$imageArray["Path"].'" class="d-block w-100" alt="..." data-bs-toggle="modal" data-bs-target="#picture'.($noOfImages-1).'Modal" />
-    // </div>';
-    //         echo '
-    // <div class="modal fade" id="picture'.($noOfImages-1).'Modal" tabindex="-1" aria-labelledby="" aria-hidden="true">
-    //     <div class="modal-dialog modal-xl modal-fullscreen-lg-down">
-    //         <div class="modal-content">
-    //             <div class="modal-header">
-    //                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-    //             </div>
-    //             <div class="modal-body">
-    //                 <img src="'.$imageArray["Path"].'" class="d-block w-100" alt="..." />
-    //             </div>
-    //             <div class="modal-footer">
-    //                 <button type="button" class="btn btn-success" data-bs-dismiss="modal">Close</button>
-    //             </div>
-    //         </div>
-    //     </div>
-    // </div>';
         }
-    ?>
+        ?>
+        </tr>
+    </table>
+    </div>
+
+    
 
     <!-- footer import was here -->
     <div class="mt-auto" style="padding-top:1em;">
